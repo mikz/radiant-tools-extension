071f3aaab7228837d445145a27cba30b269548b1
Michal Cichra <michal.cichra@gmail.com>
2011-02-06 16:15:48 +0100

    add trailing slash to urls instead of removing it (sitemap urls are with ending slash)

--- lib/radiant_tools/config.rb
+++ lib/radiant_tools/config.rb
@@ -3,7 +3,8 @@ module RadiantTools::Config
     config.gem 'rack-rewrite', :require => false
     
     config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-      r301 %r{^\/(.+)\/(\?.*)?$}, '/$1$2'
+      #r301 %r{^\/(.+)\/(\?.*)?$}, '/$1$2'
+      r301 %r{^\/([^\/?]+)(\?.*)?$}, '/$1/$2' # add trailing slash instead of removing it
     end
   end
 end
