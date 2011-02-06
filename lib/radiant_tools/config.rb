d55552cd1d0edb17fed0f397c8869b5d19f58209
Michal Cichra <michal.cichra@gmail.com>
2011-02-06 18:24:09 +0100

    redirect only GET requests

--- lib/radiant_tools/config.rb
+++ lib/radiant_tools/config.rb
@@ -3,8 +3,8 @@ module RadiantTools::Config
     config.gem 'rack-rewrite', :require => false
     
     config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-      r301 %r{^\/(.+?)(?:\/)(\?.+)$}, '/$1$2' # redirect query/? to just query?
-      r301 %r{^\/([^?]+[^\/])$}, '/$1/' # add trailing slash instead of removing it
+      r301 %r{^\/(.+?)(?:\/)(\?.+)$}, '/$1$2', :if => lambda { |env| env['REQUEST_METHOD'].to_sym == :GET } # redirect action/?query to just action?query
+      r301 %r{^\/([^?]+[^\/])$}, '/$1/', :if => lambda { |env| env['REQUEST_METHOD'].to_sym == :GET } # add trailing slash instead of removing it
     end
   end
 end
