42d9d5506113a044bbe2046d446f6ace12588d00
Michal Cichra <michal@o2h.cz>
2013-09-28 15:08:21 +0200

    fix radiant tools extension name

--- radiant_tools_extension.rb
+++ radiant_tools_extension.rb
@@ -1,17 +1,17 @@
 # Uncomment this if you reference any of your controllers in activate
 require_dependency 'application_controller'
 
-class ToolsExtension < Radiant::Extension
+class RadiantToolsExtension < Radiant::Extension
   version RadiantTools::VERSION
   description "Various tools (method, tags) for Radiant projects"
   url "http://github.com/mikz/radiant_tools"
-  
+
   extension_config do |config|
     RadiantTools::Config.update config
   end
 
   # See your config/routes.rb file in this extension to define custom routes
-  
+
   def activate
     #Radiant::Config["tools"] ||= "i18n_date i18n_archive_title reverse_breadcrumbs asset_helpers"
 
