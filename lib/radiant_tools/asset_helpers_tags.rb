fa6b31b5e8d83b9d3b4104436081d23a961498a0
Michal Cichra <michal@o2h.cz>
2013-09-28 17:11:17 +0200

    fix typo

--- lib/radiant_tools/asset_helpers_tags.rb
+++ lib/radiant_tools/asset_helpers_tags.rb
@@ -2,17 +2,17 @@ module RadiantTools::AssetHelpersTags
   def self.init
     Page.send :include, self
   end
-  
+
   include Radiant::Taggable
-  
+
   ALLOWED_HELPERS = %w{javascript stylesheet image name}
-  
+
   desc %{
     *Usage:*
 
     <pre><code><r:path_to [javascript="app.js"] [stylesheet="style.css"]/></code></pre>
   }
-  
+
   tag 'path_to' do |tag|
     unless tag.attr.length == 1
       raise "Wrong number of arguments. Accepts only ONE attribute."
@@ -31,10 +31,10 @@ module RadiantTools::AssetHelpersTags
         "javascript"
       end
     end
-    
+
     template.send("path_to_#{name}".to_sym, value).inspect
   end
-  
+
   desc %{
     *Usage:*
     <pre><code><r:stylesheet name="stylesheet.css" [media="print"]/></code></pre>
@@ -42,7 +42,7 @@ module RadiantTools::AssetHelpersTags
   tag "stylesheet" do |tag|
     template.send :stylesheet_link_tag, tag.attr.delete(:name), tag.attr
   end
-  
+
   desc %{
     *Usage:*
     <pre><code><r:javascript name="app.js" [async]/></code></pre>
@@ -50,7 +50,7 @@ module RadiantTools::AssetHelpersTags
   tag "javascript" do |tag|
     template.send :javascript_include_tag, tag.attr.delete(:name), tag.attr
   end
-  
+
   desc %{
     *Usage:*
     <pre><code><r:image [name="mage.png"] [field="Small image"]/></code></pre>
@@ -64,7 +64,7 @@ module RadiantTools::AssetHelpersTags
     end
     template.send :image_tag, name, tag.attr if name
   end
-  
-  delegate :teplate, :to => :response, :allow_nil => true
-  
+
+  delegate :template, :to => :response, :allow_nil => true
+
 end
